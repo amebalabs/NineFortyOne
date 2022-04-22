228c7f463eff1aaa4d080fe5094e7d78545ae722
Alex Mazanov <alexandr.mazanov@gmail.com>
2022-04-22 14:20:44 -0400

    Initial Commit

---
+++ NineFortyOne/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NineFortyOne
+//
+//  Created by Alexander Mazanov on 4/22/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NineFortyOne/NineFortyOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  NineFortyOneApp.swift
+//  NineFortyOne
+//
+//  Created by Alexander Mazanov on 4/22/22.
+//
+
+import SwiftUI
+
+@main
+struct NineFortyOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
